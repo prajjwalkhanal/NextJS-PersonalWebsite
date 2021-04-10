bca85228bc30df13c8582b36392fc5c4ef25e2c0
prajjwalkhanal <13815290+prajjwalkhanal@users.noreply.github.com>
2021-04-11 00:54:08 +1000

    Till AboutMe

---
+++ pages/landingpage.js
@@ -0,0 +1,63 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+import Link from "next/link";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Parallax from "components/Parallax/Parallax.js";
+// sections for this page
+import AboutMe from "pages-sections/LandingPage-Sections/AboutMe.js";
+//import MySkill from "pages-sections/LandingPage-Sections/MySkill.js";
+
+import styles from "assets/jss/nextjs-material-kit/pages/components.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Components(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        brand="NextJS Material Kit"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax image={require("assets/img/nextjs_header.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Prajjwal Khanal</h1>
+                <h3 className={classes.subtitle}>
+                  Cloud, AWS, DevOps and many more...
+                </h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <AboutMe/>
+        
+      </div>
+      <Footer />
+    </div>
+  );
+}
