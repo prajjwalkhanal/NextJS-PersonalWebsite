7694ba0a3e8b084e24822e2a23135b0a5b2cdf16
prajjwalkhanal <13815290+prajjwalkhanal@users.noreply.github.com>
2021-06-03 22:33:30 +1000

    Update

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1622325373725");
+            injectCss(link, link.href.split("?")[0] + "?unix=1622381321868");
           });
       }
     }
